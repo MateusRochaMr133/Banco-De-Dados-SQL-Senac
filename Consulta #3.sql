55e9b36fff33eebc437df6149b6791238bcb8db8
Mateus <mateusrochamr133@gmail.com>
2024-05-08 15:05:50 -0300

    Atividade Consulta SQL Senac Biblioteca

--- Consulta #3.sql
+++ Consulta #3.sql
@@ -35,9 +35,9 @@ SELECT * FROM Aluno
 
 /*Criando Tabela Professor*/
 CREATE TABLE Professor (
-areaAtuaçao VARCHAR(10) NOT NUL,
+areaAtuaçao VARCHAR(10) NOT NULL,
 salario DECIMAL(6,2) NOT NULL,
-idUsuario INT AUstarterdb22TO_INCREMENT NOT NULL,
+idUsuario INT NOT NULL,
 CONSTRAINT FK_Professor_Usuario FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario)
 );
 
@@ -49,8 +49,11 @@ SELECT * FROM Professor
 
 /*Criando Tabela Recepcionista*/
 CREATE TABLE Recepcionista (
+idRecepcionista INT AUTO_INCREMENT NOT NULL,
 idUsuario INT NOT NULL,
-CTPS VARCHAR(30)
+CONSTRAINT FK_Recepcionista_Usuario FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario),
+CTPS VARCHAR(30),
+CONSTRAINT PRIMARY KEY (idRecepcionista)
 );
 
 
@@ -62,24 +65,33 @@ SELECT * FROM Recepcionista
 /*Criando Tabela Emprestimo*/
 CREATE TABLE Emprestimo (
 idEmprestimo INT AUTO_INCREMENT NOT NULL,
-DataEmprestimo DATETIME SELECT NOW () NOT NULL,
-DataDevoluçao DATETIME SELECT NOW () NOT NULL,
-multa INT,
+DataEmprestimo DATETIME DEFAULT NOW(),
+DataDevoluçao DATETIME NOT NULL,
+dataDevoluçaoEfeita DATETIME,
+multa DECIMAL(6,2),
+idUsuario INT NOT NULL,
 idAluno INT NOT NULL,
-dataDevoluçaoEfetiva DATETIME,
 idRecepcionista INT NOT NULL,
-idUsuario INT NOT NULL
+
+CONSTRAINT FK_Emprestimo_Recepcionista_2 FOREIGN KEY (idRecepcionista) REFERENCES Recepcionista(idRecepcionista),
+
+/*CONSTRAINT FK_Aluno_Emprestimo FOREIGN KEY (idAluno) REFERENCES Aluno(idAluno)*/
+
+CONSTRAINT FK_Emprestimo_Recepcionista FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario),
+
+CONSTRAINT PRIMARY KEY (idEmprestimo)
 );
 
-SELECT * FROM Emprestiomo
+SELECT * FROM Emprestimo
 /*Fim da Tabela Emprestimo*/
 
 
 /*Criando Tabela Autor*/
 CREATE TABLE Autor (
+idAutor INT AUTO_INCREMENT NOT NULL,
 nome VARCHAR(50) NOT NULL,
 nacionalidade VARCHAR(30),
-idAutor INT NOT NULL
+CONSTRAINT PRIMARY KEY (idAutor)
 );
 
 SELECT * FROM Autor
@@ -88,7 +100,7 @@ SELECT * FROM Autor
 
 /*Criando a Tabela Livro*/
 CREATE TABLE Livro (
-idlivro INT AUTO_INCREMENT NOT NULL,
+idLivro INT AUTO_INCREMENT NOT NULL,
 nome VARCHAR(100) NOT NULL,
 tomo VARCHAR(10) NOT NULL,
 genero VARCHAR(15) NOT NULL,
@@ -96,8 +108,14 @@ qtdPaginas INT,
 idioma VARCHAR(20) NOT NULL,
 ano INT,
 edicao INT,
-idAutor INT,
-idEditora
+idAutor INT NOT NULL,
+idEditora INT NOT NULL,
+
+CONSTRAINT FK_Autor_Livro FOREIGN KEY (idAutor) REFERENCES Autor(idAutor),
+
+CONSTRAINT FK_Livro_Editora FOREIGN KEY (idEditora) REFERENCES Editora(idEditora),
+
+CONSTRAINT PRIMARY KEY (idLivro)
 );
 
 SELECT * FROM Livro
@@ -110,7 +128,8 @@ nome VARCHAR(30) NOT NULL,
 celular VARCHAR(14) NOT NULL,
 email VARCHAR(50) NOT NULL,
 responsavel VARCHAR(50) NOT NULL,
-isbn CHAR(10)
+isbn CHAR(10),
+CONSTRAINT PRIMARY KEY (idEditora)
 );
 
 SELECT * FROM Editora
@@ -121,6 +140,9 @@ SELECT * FROM Editora
 CREATE TABLE LivroAutor (
 idLivro INT NOT NULL,
 idAutor INT NOT NULL,
+CONSTRAINT FK_Livro_Autor FOREIGN KEY (idLivro) REFERENCES Livro(idLivro),
+
+CONSTRAINT FK_Livro_Autores FOREIGN KEY (idAutor) REFERENCES Autor(idAutor)
 );
 
 SELECT * FROM LivroAutor
